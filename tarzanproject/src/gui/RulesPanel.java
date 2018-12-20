b4a5ede817b4068bc22a208769d61964fee94211
faust <faust@DESKTOP-3V2B2T6>
2018-12-20 10:55:22 +0100

    rules

--- tarzanproject/src/gui/RulesPanel.java
+++ tarzanproject/src/gui/RulesPanel.java
@@ -1,7 +1,8 @@
 package gui;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.text.JTextComponent;
 
 /**
  * @author Faustine & Martina
@@ -13,15 +14,25 @@ import javax.swing.JPanel;
 
 public class RulesPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JLabel rules;
+	private JTextComponent rules;
 
 	/**
 	 * Constructor. TODO: write rules.
 	 */
 	public RulesPanel() {
 		// just text with rules
-		String text = " THE RULES WILL COME HERE "; // get text from file
-		rules = new JLabel(text);
+		String text = " You play Tarzan and your goal is to save Jane. \n "
+				+ "You start with an initial energy of 500 and the game is lost if you have no more. \n"
+				+ "To increase your energy, you can take Kavuru's pills. \n"
+				+ "Wlaking on grass costs you less energy than swimming. \n"
+				+ "Eating bananas increases your endurance, so decreases your energy loss per move. \n"
+				+ "Fighting jaguars costs you energy, but you need to have fought some before saving Jane. \n"
+				+ "Winning a fight is somewhat random, but the chances increase if your stronger. \n"
+				+ "Picking up knives increase your strength. \n"
+				+ "Once your are sufficiently strong and endurant, and that you have killed enough jaguars, \n"
+				+ "you can go find Jane and win the game. \n \n "
+				+ "Good luck! \n"; // get text from file?
+		rules = new JTextArea(text);
 		this.add(rules);
 	}
 }
